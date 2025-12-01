639f7d22a7e413f443e76907b0b43ca0a796a038
suqingdong <1078595229@qq.com>
2025-12-01 17:10:49 +0800

    update serializer

--- backend/apps/pubmed/serializers.py
+++ backend/apps/pubmed/serializers.py
@@ -15,5 +15,9 @@ class PubmedArticleSerializer(serializers.ModelSerializer):
             'factor',
             'jcr',
             'journal',
+            'pagination',
+            'volume',
             'authors',
+            'doi',
+            'pmc',
         ]
